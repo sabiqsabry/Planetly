0c7dab08458dd4247c6fe705ae22ddd61bdf18d6
Sabiq Sabry <sabiqsabry48@gmail.com>
2023-03-20 03:10:43 +0000

    Initial Commit

---
+++ Planetly/PlanetlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetlyApp.swift
+//  Planetly
+//
+//  Created by Sabiq Sabry on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlanetlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
